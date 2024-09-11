d9f0b7c7d3879d789883f3d9b4d66829755bba76
ioann <ivan.karmalyk2017@gmail.com>
2024-09-11 09:20:37 +0300

    added sanctum auth to tasks api

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('tasks', TaskController::class);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('tasks', TaskController::class);
+});
 
